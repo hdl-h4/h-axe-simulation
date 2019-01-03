d8934046187db89948dd2264289763c4ec11619f
curiosityyy <24curiosity@gmail.com>
2019-01-03 20:21:42 +0800

    [cmake] add CMakeLists.txt

--- src/main.cc
+++ src/main.cc
@@ -11,3 +11,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+#include "worker.h"
+#include <iostream>
+
+int main(int argc, char **argv) {
+
+  std::cout << " yo!\n";
+  return 0;
+}
+
