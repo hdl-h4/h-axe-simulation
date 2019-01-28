4b61242df94b4e456ac8462f06d758110495f0ae
byli <24curiosity@gmail.com>
2019-01-28 15:11:40 +0800

    [job_manager] add subgraph dependencies

--- src/job_manager.h
+++ src/job_manager.h
@@ -95,80 +95,81 @@ public:
                       return event_vector;
                     });
 
-    RegisterHandler(
-        EventType::TASK_FINISH, [&](std::shared_ptr<Event> event)
-                                    -> std::vector<std::shared_ptr<Event>> {
-          std::vector<std::shared_ptr<Event>> event_vector;
+    RegisterHandler(EventType::TASK_FINISH, [&](std::shared_ptr<Event> event)
+                                                -> std::vector<
+                                                    std::shared_ptr<Event>> {
+      std::vector<std::shared_ptr<Event>> event_vector;
 
-          double time = event->GetTime();
-          int event_id = event->GetEventID();
-          std::shared_ptr<TaskFinishEvent> task_finish_event =
-              std::static_pointer_cast<TaskFinishEvent>(event);
-          auto &finish_task = task_finish_event->GetShardTask();
-          auto subgraph_id = shard_task_to_subgraph_[ShardTaskID{
-              finish_task.GetTaskID(), finish_task.GetShardID()}];
-          auto &sg = job_.GetSubGraphs().at(subgraph_id);
+      double time = event->GetTime();
+      int event_id = event->GetEventID();
+      std::shared_ptr<TaskFinishEvent> task_finish_event =
+          std::static_pointer_cast<TaskFinishEvent>(event);
+      auto &finish_task = task_finish_event->GetShardTask();
+      auto subgraph_id = shard_task_to_subgraph_[ShardTaskID{
+          finish_task.GetTaskID(), finish_task.GetShardID()}];
+      auto &sg = job_.GetSubGraphs().at(subgraph_id);
 
-          DLOG(INFO) << "TASK FINISH : subgraph id = " << subgraph_id
-                     << ", task id = " << finish_task.GetTaskID()
-                     << ", shard id = " << finish_task.GetShardID();
+      DLOG(INFO) << "TASK FINISH : subgraph id = " << subgraph_id
+                 << ", task id = " << finish_task.GetTaskID()
+                 << ", shard id = " << finish_task.GetShardID()
+                 << ", worker id = " << sg.GetWorkerID();
 
-          // task finish update resource
+      // task finish update resource
 
-          std::vector<std::shared_ptr<Event>> events =
-              workers_->at(sg.GetWorkerID())
-                  .TaskFinish(time, event_id, finish_task);
-          event_vector.insert(event_vector.end(), events.begin(), events.end());
-          users_->at(job_.GetUserID()).TaskFinish(time, finish_task);
+      std::vector<std::shared_ptr<Event>> events =
+          workers_->at(sg.GetWorkerID())
+              .TaskFinish(time, event_id, finish_task);
+      event_vector.insert(event_vector.end(), events.begin(), events.end());
+      users_->at(job_.GetUserID()).TaskFinish(time, finish_task);
 
-          subgraph_finished_task_[subgraph_id]++;
-          // subgraph finish update memory
-          if (sg.GetShardTasks().size() ==
-              subgraph_finished_task_[subgraph_id]) {
-            workers_->at(sg.GetWorkerID()).SubGraphFinish(time, sg.GetMemory());
-            users_->at(job_.GetUserID()).SubGraphFinish(time, sg.GetMemory());
-            DLOG(INFO) << "finish subgraph: " << subgraph_id
-                       << " of job id: " << sg.GetJobID();
-            finish_subgraph_num_++;
-            if (finish_subgraph_num_ == job_.GetSubGraphs().size()) {
-              event_vector.push_back(std::make_shared<JobFinishEvent>(
-                  EventType::JOB_FINISH, time, 0, kScheduler, job_.GetJobID(),
-                  job_.GetSubmissionTime()));
-            }
-          }
+      subgraph_finished_task_[subgraph_id]++;
+      // subgraph finish update memory
+      if (sg.GetShardTasks().size() == subgraph_finished_task_[subgraph_id]) {
+        workers_->at(sg.GetWorkerID()).SubGraphFinish(time, sg.GetMemory());
+        users_->at(job_.GetUserID()).SubGraphFinish(time, sg.GetMemory());
+        DLOG(INFO) << "finish subgraph: " << subgraph_id
+                   << " of job id: " << sg.GetJobID();
+        finish_subgraph_num_++;
+        if (finish_subgraph_num_ == job_.GetSubGraphs().size()) {
+          event_vector.push_back(std::make_shared<JobFinishEvent>(
+              EventType::JOB_FINISH, time, 0, kScheduler, job_.GetJobID(),
+              job_.GetSubmissionTime()));
+        }
+      }
 
-          for (auto &child : finish_task.GetChildren()) {
-            dep_finish_counter_[child]++;
-            if (dep_finish_counter_[child] == dep_counter_[child]) {
-              if (job_.GetSubGraphs()
-                      .at(shard_task_to_subgraph_[child])
-                      .GetWorkerID() == -1) {
-                for (const auto &req :
-                     GenerateResourceRequest(EventType::TASK_FINISH, child)) {
-                  event_vector.push_back(
-                      std::make_shared<NewTaskReqEvent>(NewTaskReqEvent(
-                          EventType::NEW_TASK_REQ, time, 0, kScheduler, req)));
-                }
-              } else {
-                ShardTask task = id_to_shard_task_[child];
-                std::vector<std::shared_ptr<Event>> update_event_vector =
-                    workers_
-                        ->at(job_.GetSubGraphs()
-                                 .at(shard_task_to_subgraph_[child])
-                                 .GetWorkerID())
-                        .PlaceNewTask(time, task);
-                event_vector.insert(event_vector.end(),
-                                    update_event_vector.begin(),
-                                    update_event_vector.end());
+      for (auto &child : finish_task.GetChildren()) {
+        dep_finish_counter_[child]++;
+        int child_subgraph_id = shard_task_to_subgraph_[child];
+        subgraph_dep_finish_counter_[child_subgraph_id]++;
+        if (dep_finish_counter_[child] == dep_counter_[child]) {
+          if (job_.GetSubGraphs().at(child_subgraph_id).GetWorkerID() == -1) {
+            if (subgraph_dep_finish_counter_[child_subgraph_id] ==
+                subgraph_dep_counter_[child_subgraph_id]) {
+              for (const auto &req :
+                   GenerateResourceRequest(EventType::TASK_FINISH, child)) {
+                event_vector.push_back(
+                    std::make_shared<NewTaskReqEvent>(NewTaskReqEvent(
+                        EventType::NEW_TASK_REQ, time, 0, kScheduler, req)));
               }
             }
+          } else {
+            ShardTask task = id_to_shard_task_[child];
+            std::vector<std::shared_ptr<Event>> update_event_vector =
+                workers_
+                    ->at(
+                        job_.GetSubGraphs().at(child_subgraph_id).GetWorkerID())
+                    .PlaceNewTask(time, task);
+            event_vector.insert(event_vector.end(), update_event_vector.begin(),
+                                update_event_vector.end());
           }
+        }
+      }
 
-          // create resource available event
-          event_vector.push_back(std::make_shared<ResourceAvailableEvent>(
-              EventType::RESOURCE_AVAILABLE, time, 0, kScheduler));
-          return event_vector;
-        });
+      // create resource available event
+      event_vector.push_back(std::make_shared<ResourceAvailableEvent>(
+          EventType::RESOURCE_AVAILABLE, time, 0, kScheduler));
+      return event_vector;
+    });
   }
 
   std::vector<ResourceRequest>
@@ -190,6 +191,7 @@ public:
       }
     } else {
       int id = shard_task_to_subgraph_[shard_task];
+      DLOG(INFO) << "subgraph id : " << id;
       if (subgraph_to_req_[id] == 1) {
         return req_vector;
       }
@@ -216,6 +218,16 @@ public:
         }
       }
     }
+    for (int i = 0; i < job_.GetSubGraphs().size(); i++) {
+      const auto &sg = job_.GetSubGraphs().at(i);
+      for (const auto &st : sg.GetShardTasks()) {
+        for (const auto &child : st.GetChildren()) {
+          if (shard_task_to_subgraph_[child] != i) {
+            subgraph_dep_counter_[shard_task_to_subgraph_[child]]++;
+          }
+        }
+      }
+    }
   }
 
 private:
@@ -225,8 +237,10 @@ private:
   std::map<ShardTaskID, ShardTask> id_to_shard_task_;
   std::map<ShardTaskID, int> shard_task_to_subgraph_;
   std::unordered_map<int, bool> subgraph_to_req_;
-  std::map<ShardTaskID, int> dep_counter_;
-  std::map<ShardTaskID, int> dep_finish_counter_;
+  std::map<ShardTaskID, int> dep_counter_;        // for task
+  std::map<ShardTaskID, int> dep_finish_counter_; // for task
+  std::map<int, int> subgraph_dep_counter_;
+  std::map<int, int> subgraph_dep_finish_counter_;
   std::shared_ptr<std::vector<Worker>> workers_;
   std::shared_ptr<std::vector<User>> users_;
 }; // namespace simulation
